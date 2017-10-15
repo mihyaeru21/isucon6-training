2445c1f958872e969cbec59c5918082c91097ff0
Mihyaeru <mihyaeru21@gmail.com>
2017-10-15 17:31:05 +0900

    config

--- restart.sh
+++ restart.sh
@@ -4,5 +4,9 @@ sudo cp my.cnf /etc/mysql/my.cnf
 sudo cp mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
 sudo cp nginx.conf /etc/nginx/nginx.conf
 sudo nginx -t
+
+sudo rm /var/log/mysql/mysql-slow.log
+sudo rm /var/log/nginx/access.log
+
 sudo systemctl daemon-reload && sudo systemctl restart nginx.service mysql.service isutar.perl.service isuda.perl.service
 
